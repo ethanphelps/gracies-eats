b100b05a3cbb919bcf21c116545180ccb49f4980
ethanphelps <ethanrphelpsapps@gmail.com>
2022-07-25 23:30:17 -0500

    delayed reaction text input and start of data modeling

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { ChangeEvent, FormEvent } from "react";
 import "./landing.scss";
 
 export const Landing: React.FC = (): React.ReactElement => {
+
+  /**
+   * calls setTimeout with a delay and also clears the previous timeout to avoid
+   * duplicate callback calls for one set of changes. Every time a character is
+   * entered or deleted, the timer until callback gets called is reset. Once no
+   * characters have been typed for delay ms, then callback is called
+   */
+  const TextInputDelayedReaction = ({
+    delay,
+    callback,
+  }: {
+    delay: number;
+    callback: (e: string) => void;
+  }): React.ReactElement => {
+
+    let timeoutId = 0;
+    const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
+      if (timeoutId) window.clearTimeout(timeoutId);
+      timeoutId = window.setTimeout(() => {
+        callback(e.target.value);
+      }, delay);
+    };
+
+    return (
+      <div className="input-container">
+        <input
+          onInput={handleInput}
+          type="text"
+          id="delayed-text"
+          name="delayed-text"
+        />
+      </div>
+    );
+  };
+
   interface RecipeCardProps {
-    id?: number,
+    id?: number;
     src: string;
     title: string;
     time: string;
@@ -51,24 +86,6 @@ export const Landing: React.FC = (): React.ReactElement => {
       title: "Kale Salad",
       time: "25 min",
     },
-    {
-      id: 5,
-      src: "kale_salad.jpg",
-      title: "Kale Salad",
-      time: "25 min",
-    },
-    {
-      id: 6,
-      src: "kale_salad.jpg",
-      title: "Kale Salad",
-      time: "25 min",
-    },
-    {
-      id: 7,
-      src: "kale_salad.jpg",
-      title: "Kale Salad",
-      time: "25 min",
-    },
   ];
 
   return (
@@ -79,13 +96,14 @@ export const Landing: React.FC = (): React.ReactElement => {
         <RecipeCard src="kale_salad.jpg" title="Kale Salad" time="25 min"/> */}
         {data.map((recipe) => (
           <RecipeCard
-            key={recipe.id} 
+            key={recipe.id}
             src={recipe.src}
             title={recipe.title}
             time={recipe.time}
           />
         ))}
       </div>
+      <TextInputDelayedReaction delay={500} callback={console.log} />
     </div>
   );
 };
