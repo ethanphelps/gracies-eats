f6ce90d016bdcf3b5e050bc9dc12e7589a018cbd
ethanphelps <ethanrphelpsapps@gmail.com>
2022-08-04 00:21:26 -0400

    more fields

--- src/pages/add-recipe/RecipeForm.tsx
+++ src/pages/add-recipe/RecipeForm.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './RecipeForm.scss'
+import "./RecipeForm.scss";
 import { IconButton } from "../landing/landing";
 
 export const RecipeForm: React.FC = (): React.ReactElement => {
@@ -8,31 +8,48 @@ export const RecipeForm: React.FC = (): React.ReactElement => {
     placeholder: string;
     initialHeight?: string;
   }
-  const FormInput = ({title, placeholder, initialHeight }: FormInputProps): React.ReactElement => {
-    const inlineStyle = initialHeight ? initialHeight : '';
+  const FormInput = ({
+    title,
+    placeholder,
+    initialHeight,
+  }: FormInputProps): React.ReactElement => {
+    const inlineStyle = initialHeight ? { height: initialHeight } : {};
+    const wrapMode = initialHeight ? "on" : "off";
     return (
-        <div className="form-item" >
-          <h5 className="form-label"> {title} </h5>
-          <input type="text" className="form-input" placeholder={placeholder}></input>
-        </div>
-    )
-  }
+      <div className="form-item">
+        <h5 className="form-label"> {title} </h5>
+        <textarea
+          className="form-input"
+          placeholder={placeholder}
+          style={inlineStyle}
+          wrap={wrapMode}
+        ></textarea>
+      </div>
+    );
+  };
 
   return (
     // <div className="recipe-form-container">
-      <form id="recipe-form">
-        <header id="form-title">Create a New Recipe</header>
-        <section className="form-row">
-          <FormInput title="Title" placeholder="My Delicious Recipe" />
-        </section>
-        <section className="form-row">
-          <FormInput title="Prep Time" placeholder="About 30 min" />
-          <FormInput title="Cook Time" placeholder="About 45 min" />
-        </section>
-        <section className="form-row">
-          <FormInput title="Description" placeholder="My Delicious Recipe" />
-        </section>
-      </form>
+    <form id="recipe-form">
+      <header id="form-title">Create a New Recipe</header>
+      <section className="form-row">
+        <FormInput title="Title" placeholder="My Delicious Recipe" />
+      </section>
+      <section className="form-row">
+        <FormInput title="Prep Time" placeholder="About 30 min" />
+        <FormInput title="Cook Time" placeholder="About 45 min" />
+      </section>
+      <section className="form-row">
+        <FormInput
+          title="Description"
+          placeholder="A short, gustatory description in case you forget what the recipe is like!"
+          initialHeight="100px"
+        />
+      </section>
+      <section className="form-row">
+        <FormInput title="Serves" placeholder="2 people" />
+      </section>
+    </form>
     // </div>
-  )
-}
+  );
+};
