43dacdc76731b7de874759915655dc3039ddee11
ethanphelps <ethanrphelpsapps@gmail.com>
2022-08-05 00:46:49 -0400

    ingredients list created

--- src/pages/add-recipe/RecipeForm.tsx
+++ src/pages/add-recipe/RecipeForm.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./RecipeForm.scss";
 import { IconButton } from "../landing/landing";
 
@@ -7,14 +7,21 @@ interface FormInputProps {
   placeholder: string;
   initialHeight?: string;
   multiLine?: boolean;
+  flexGrow?: number;
+  flexShrink?: number;
+  maxWidth?: string;
 }
 const FormInput = ({
   title,
   placeholder,
   initialHeight,
   multiLine = false,
+  flexGrow = 1,
+  flexShrink = 1,
+  maxWidth = null,
 }: FormInputProps): React.ReactElement => {
-  const inlineStyle = initialHeight ? { height: initialHeight } : {};
+  let inlineStyle: any = initialHeight ? { height: initialHeight } : {};
+  inlineStyle.maxWidth = maxWidth;
 
   // display textarea or input based on multiLine prop
   const input = multiLine ? (
@@ -24,21 +31,70 @@ const FormInput = ({
       style={inlineStyle}
     ></textarea>
   ) : (
-    <input className="form-input" placeholder={placeholder}></input>
+    <input
+      className="form-input"
+      style={inlineStyle}
+      placeholder={placeholder}
+    ></input>
   );
-
   // conditionally display label if title provided
   const label = title ? <h5 className="form-label">{title}</h5> : null;
-
+  const outerStyle = {
+    flexGrow: flexGrow,
+    flexShrink: flexShrink,
+  };
   return (
-    <div className="form-item">
+    <div className="form-item" style={outerStyle}>
       {label}
       {input}
     </div>
   );
 };
 
+/**
+ * Component for adding new row of a specific component type
+ */
+interface NewButtonProps {
+  callback: (event: React.MouseEvent) => void;
+}
+const NewButton = ({ callback }: NewButtonProps): React.ReactElement => {
+  return (
+    <button className="new-button" onClick={callback}>
+      <span>+</span>
+      <span>New</span>
+    </button>
+  );
+};
+
+interface Ingredient {
+  id: number;
+  name: string;
+  quantity: number;
+}
+interface InstructionStep {
+  id: number;
+  description: string;
+  prePrep: boolean;
+}
+
 export const RecipeForm: React.FC = (): React.ReactElement => {
+  // add ingredient components here whenever new button pressed
+  // const ingredientList: Ingredient[] = [
+  //   { id: 0, name: '', quantity: 0 }
+  // ]
+  const [ingredientList, setIngredientList] = useState<Ingredient[]>([
+    { id: 0, name: "", quantity: 0 },
+  ]);
+  const IngredientRow = () => {
+    return (
+      <section className="form-list-row">
+        <span>◈</span>
+        <FormInput placeholder="Quantity" flexShrink={2} />
+        <FormInput placeholder="Name" flexGrow={12} />
+      </section>
+    );
+  };
+
   return (
     // <div className="recipe-form-container">
     <form id="recipe-form">
@@ -61,15 +117,31 @@ export const RecipeForm: React.FC = (): React.ReactElement => {
       <section className="form-row">
         <FormInput title="Serves" placeholder="2 people" />
       </section>
-      <section className="form-row">
+
+      <section className="form-list">
+        <h5 className="form-label">Ingredients</h5>
+        {ingredientList.map((ingredient) => (
+          <IngredientRow key={ingredient.id} />
+        ))}
+        <NewButton
+          callback={(ev) => {
+            ev.preventDefault(); // don't reload the page
+            console.log("new button clicked!");
+            // add new ingredient to list
+            setIngredientList((ingredients) => [
+              ...ingredients,
+              { id: ingredientList.length, name: "", quantity: 0 },
+            ]);
+            console.log(ingredientList);
+          }}
+        />
+      </section>
+      {/* <section className="form-row">
         <span>◈</span>
-        <FormInput title="Ingredients" placeholder="Quanity" />
+        <FormInput placeholder="Quanity" />
         <FormInput placeholder="Name" />
-      </section>
-      <section className="form-row">
-        <span>+</span>
-        <span>New</span>
-      </section>
+      </section> */}
+      <section className="form-row"></section>
     </form>
     // </div>
   );
