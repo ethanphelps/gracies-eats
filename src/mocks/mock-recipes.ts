0d4afedfc38b94b80b4dc0bc1cd63e3e5313b3c4
ethanphelps <ethanrphelpsapps@gmail.com>
2022-11-14 00:11:03 -0600

    recipe view page loads generic data and there is a loading spinner

--- src/mocks/mock-recipes.ts
+++ src/mocks/mock-recipes.ts
@@ -12,10 +12,11 @@ export const recipes: Recipe[] = [
             { id: 0, name: 'garlic cloves', quantity: 3},
             { id: 1, name: 'green onions', quantity: 5},
             { id: 2, name: 'bell pepper', quantity: 1},
+            { id: 3, name: 'pack of noodles', quantity: 1},
         ],
         instructions: [
             { id: 0, description: 'preheat oven', prePrep: true },
-            { id: 1, description: 'stir veggies in the wok', prePrep: false },
+            { id: 1, description: 'stir veggies in the wok until lightly brown', prePrep: false },
         ]
     },
     {
@@ -36,6 +37,8 @@ export const recipes: Recipe[] = [
             { id: 1, description: 'start rice cooker', prePrep: true },
             { id: 2, description: 'cut brussel sprouts', prePrep: false },
             { id: 3, description: 'cook brussel sprouts in air fryer', prePrep: false },
+            { id: 4, description: 'bake cauliflower in the oven', prePrep: false },
+            { id: 5, description: 'plate it up', prePrep: false },
         ]
     }
 ]
--- src/pages/recipe/RecipeView.tsx
+++ src/pages/recipe/RecipeView.tsx
@@ -1,28 +1,47 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState, CSSProperties } from "react";
 import { RouteContext } from "../../context";
 import { Ingredient, InstructionStep, Recipe } from "../../models/models";
 import "./recipe.scss";
 import { recipes } from "../../mocks/mock-recipes";
+import { PacmanLoader } from "react-spinners";
 
-const IngredientRow: React.FC = ({
+const spinnerCSS: CSSProperties = {
+    top: "-30px",
+    left: "-35px"
+}
+
+const IngredientRow = ({
     ingredient,
 }: {
     ingredient: Ingredient;
 }): React.ReactElement => {
     return (
         <section className="list-row">
-            <span>◈</span>
-            <span>{ingredient.quantity}</span>
-            <span>{ingredient.name}</span>
+            <div className="list-icon">◈</div>
+            <div>{ingredient.quantity.toString() + ' ' + ingredient.name}</div>
+        </section>
+    );
+};
+const InstructionRow = ({
+    instruction,
+}: {
+    instruction: InstructionStep;
+}): React.ReactElement => {
+    return (
+        <section className="list-row">
+            <div className="list-number">{instruction.id}.</div>
+            <div>{instruction.description}</div>
         </section>
     );
 };
 
-export const RecipeView: React.FC = (): React.ReactElement => {
+const Divider = (): React.ReactElement => <div className="divider"></div>
+
+
+export const RecipeView = (): React.ReactElement => {
     const routeContext = useContext(RouteContext);
     const [toggle, setToggle] = useState("instructions");
     const [recipeData, setRecipeData] = useState<Recipe | null>(null);
-    // let recipeData: Recipe | null = null;
 
     // make network call to get recipe data here
     useEffect(() => {
@@ -30,8 +49,8 @@ export const RecipeView: React.FC = (): React.ReactElement => {
         for(const recipe of recipes) {
             if(recipe.id == recipeId) {
                 console.log(`Recipe ${recipeId} found!`)
-                // recipeData = recipe;
-                setRecipeData(recipe);
+                // setRecipeData(recipe);
+                setTimeout(() => setRecipeData(recipe), 200)
             }
         }
         if(!recipeData) {
@@ -39,58 +58,74 @@ export const RecipeView: React.FC = (): React.ReactElement => {
         }
     }, []);
 
-    const instructionsClass =
-        toggle === "instructions" ? "toggle-text selected" : "toggle-text";
-    const ingredientsClass =
-        toggle === "ingredients" ? "toggle-text selected" : "toggle-text";
+    const instructionsClass = toggle === "instructions" ? "toggle-text selected" : "toggle-text";
+    const ingredientsClass = toggle === "ingredients" ? "toggle-text selected" : "toggle-text";
 
-    console.log("Available dynamic parameters: ", routeContext.params);
-
-    return (
-        <div id="recipe-container">
-            <header id="recipe-title">{recipeData ? recipeData.title : 'loading'}</header>
-            <section id="recipe-details">
-                <div className="info-item">
-                    Serves: <span>{2}</span>
-                </div>
-                <div className="info-item">
-                    Prep: <span>{"15 min"}</span>
-                </div>
-                <div className="info-item">
-                    Cook: <span>{"30 min"}</span>
-                </div>
-            </section>
-            <section id="recipe-description">
-                <p>
-                    {
-                        "Yummy drunken noodles that get cooked in our big wok pan! Its filled with love and heat."
-                    }
-                </p>
-            </section>
-            {/* <span>This is the recipe page for id {routeContext.params['recipeId']} with random id {routeContext.params['editParam']}</span> */}
-            <div id="ingredients-instructions-toggle">
-                <div id="instructions" className="toggle-item">
-                    <div
-                        className={instructionsClass}
-                        onClick={() => setToggle("instructions")}
-                    >
-                        Instructions
-                    </div>
-                </div>
-                <div id="ingredients" className="toggle-item">
-                    <div
-                        className={ingredientsClass}
-                        onClick={() => setToggle("ingredients")}
-                    >
-                        Ingredients
-                    </div>
-                </div>
+    if(!recipeData) {
+        return (
+            <div id="loader-container">
+                <PacmanLoader size={30} color="#3b427d" cssOverride={spinnerCSS}></PacmanLoader>
             </div>
+        );
+    } else {
+        return (
+            <div id="recipe-container">
+                <header id="recipe-title">{recipeData.title}</header>
+                <section id="recipe-details">
+                    <div className="info-item">
+                        Serves: <span>{recipeData.serves}</span>
+                    </div>
+                    <div className="info-item">
+                        Prep: <span>{recipeData.prepTime}</span>
+                    </div>
+                    <div className="info-item">
+                        Cook: <span>{recipeData.cookTime}</span>
+                    </div>
+                </section>
+                <section id="recipe-description">
+                    <p> { recipeData.description } </p>
+                </section>
+                <div id="ingredients-instructions-toggle">
+                    <div id="instructions" className="toggle-item">
+                        <div
+                            className={instructionsClass}
+                            onClick={() => setToggle("instructions")}
+                        >
+                            Instructions
+                        </div>
+                    </div>
+                    <div id="ingredients" className="toggle-item">
+                        <div
+                            className={ingredientsClass}
+                            onClick={() => setToggle("ingredients")}
+                        >
+                            Ingredients
+                        </div>
+                    </div>
+                </div>
 
-            <section id="instructions-ingredients-container">
-                <div id="instructions" className="item-list"></div>
-                <div id="ingredients" className="item-list"></div>
-            </section>
-        </div>
-    );
+                <section id="instructions-ingredients-container">
+                    {toggle === "instructions" ? 
+                        <div id="instructions" className="item-list">
+                            {recipeData.instructions.map((instruction: InstructionStep, i: number) => (
+                                <>
+                                    <InstructionRow key={instruction.id} instruction={instruction}/>
+                                    {i == recipeData.instructions.length - 1 ? null : <Divider />}
+                                </>
+                            ))}
+                        </div>
+                    :
+                        <div id="ingredients" className="item-list">
+                            {recipeData.ingredients.map((ingredient: Ingredient, i: number) => (
+                                <>
+                                    <IngredientRow key={ingredient.id} ingredient={ingredient}/>
+                                    {i == recipeData.ingredients.length - 1 ? null : <Divider />}
+                                </>
+                            ))}
+                        </div>
+                    }
+                </section>
+            </div>
+        );
+    }
 };
